c89f025c479b0fb88a84a8713db2fb3bb6067ac6
Mila Brovkina <mila@m.com>
2019-06-19 23:30:51 +0200

    basic variables and methods in Python

--- HelloWord.py
+++ HelloWord.py
@@ -1,1 +1,34 @@
 print("Hello world")
+a = 5
+b = 105
+my_variable = 10
+
+string_variable = "Python"
+single_quotes = 'string can have single quotes'
+
+## print(single_quotes)
+# print(my_variable)
+
+var1 = 5
+var2 = 5
+num1 = 2
+num2 = 8
+
+result = num1*num2
+
+print(result)
+
+
+def my_print_method(parameter):
+    print(parameter)
+
+
+my_print_method('single_quotes')
+
+
+def my_mulptiply_method(number_one, number_two):
+    return number_one*number_two
+
+
+result = my_mulptiply_method(7, 90)
+print(result)
